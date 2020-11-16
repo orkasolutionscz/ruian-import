54eb0212e4f9c530fac7f10f4edf24a72d4faed9
jiri.fait <fait@orkasolutions.cz>
2020-11-16 21:33:17 +0100

    prcovni verye 2

--- autogen/settings.py
+++ autogen/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'autogen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.environ.get('DB_HOST', default='192.168.43.10'),
+        'NAME': os.environ.get('DB_NAME', default='lms'),
+        'USER': os.environ.get('DB_USER', default='devroot'),
+        'PASSWORD': os.environ.get('DB_PASS', default='fbJWd8pDGM7BQ5'),
+        'PORT': os.environ.get('DB_PORT', default=3306),
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     },
     'ruian': {
         'ENGINE': 'django.db.backends.sqlite3',
